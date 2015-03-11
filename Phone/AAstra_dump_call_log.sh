59778463952f299d56a248578d6dd9d07f4ddd62
azarane <s.bebonaubert@gmail.com>
2015-03-11 17:32:58 +0100

    Update AAstra_dump_call_log.sh

--- Phone/AAstra_dump_call_log.sh
+++ Phone/AAstra_dump_call_log.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Enter the IP address"; 
-read adresse; 
+read add; 
 echo "Enter the password"; 
-read mdp; 
-nom=$(timeout 3s curl --data passw=$mdp -L http://$adresse/callLog.asp | grep printSelectedNumCLForm | cut -d \> -f 5 | cut -d \< -f 1 > nom.txt); 
-numero=$(timeout 3s curl --data passw=$mdp -L http://$adresse/callLog.asp | grep printSelectedNumCLForm | cut -d = -f 4 | cut -d \" -f 1 > numero.txt); 
-date=$(timeout 3s curl --data passw=$mdp -L http://$adresse/callLog.asp | grep 180 | cut -d \> -f 3 | cut -d \< -f 1 > date.txt); 
-duree=$(timeout 3s curl --data passw=$mdp -L http://$adresse/callLog.asp | grep \"80 | cut -d \> -f 3 | cut -d \< -f 1 > duree.txt); 
-mode=$(timeout 3s curl --data passw=$mdp -L http://$adresse/callLog.asp | grep img | cut -d \" -f 4 | cut -d . -f 1 > mode.txt); 
-tableau=$(paste nom.txt numero.txt date.txt duree.txt mode.txt > dump_journal_$adresse.txt);
+read passw; 
+$(timeout 3s curl --data passw=$passw -L http://$add/callLog.asp | grep printSelectedNumCLForm | cut -d \> -f 5 | cut -d \< -f 1 > nom.txt); 
+$(timeout 3s curl --data passw=$passw -L http://$add/callLog.asp | grep printSelectedNumCLForm | cut -d = -f 4 | cut -d \" -f 1 > numero.txt); 
+$(timeout 3s curl --data passw=$passw -L http://$add/callLog.asp | grep 180 | cut -d \> -f 3 | cut -d \< -f 1 > date.txt); 
+$(timeout 3s curl --data passw=$passw -L http://$add/callLog.asp | grep \"80 | cut -d \> -f 3 | cut -d \< -f 1 > duree.txt); 
+$(timeout 3s curl --data passw=$ -L http://$add/callLog.asp | grep img | cut -d \" -f 4 | cut -d . -f 1 > mode.txt); 
+$(paste nom.txt numero.txt date.txt duree.txt mode.txt > dump_call_log_$add.txt);
